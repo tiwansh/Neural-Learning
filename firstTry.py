49e9b309ad60579e6d049fa66d7d21d4219e0580
tiwansh <at.slimshady@gmail.com>
2018-02-05 02:35:35 +0530

    Next Step : Data Manipulation

--- firstTry.py
+++ firstTry.py
@@ -7,13 +7,13 @@ import matplotlib.pyplot as plt
 train_df = pd.read_csv('train.csv')
 test_df = pd.read_csv('test.csv')
 
-#1 Take a look at data
+#1 -----------------------Take a look at data----------------------
 #print train_df.info()
 
-#2 Describe the data
+#2 -----------------------Describe the data------------------------
 #print train_df.describe()
 
-#3 Test the correlation
+#3 -----------------------Test the correlation---------------------
 #print train_df[['Sex', 'Survived']].groupby(['Sex'], as_index = False).mean()
 #print train_df[['Pclass','Survived']].groupby(['Pclass'], as_index = False).mean()
 #print train_df[["SibSp", "Survived"]].groupby(['SibSp'], as_index=False).mean().sort_values(by = 'Survived', ascending = False)
@@ -21,9 +21,20 @@ test_df = pd.read_csv('test.csv')
 #print train_df[['Fare','Survived']].groupby(['Fare'], as_index = False).mean() - no insight - scattered data
 #print train_df[['Age','Survived']].groupby(['Age'], as_index = False).mean() - no insight again - Scattered data
 
-#4 Plot histograms/scatter graph/kdeplot in cases when data is fucking scattered
+#4 ---------------------Plot histograms/scatter graph/kdeplot in cases when data is scattered--------------
 #grid = sns.FacetGrid(train_df, col = 'Survived');
 #grid.map(plt.hist, 'Fare', bins = 20);  #histogram needs bins attribute
 #grid.map(plt.scatter, 'Fare','Pclass'); #no bins in scatter 
 #grid.map(sns.kdeplot, 'Pclass', 'Fare');
-plt.show()
+#grid = sns.FacetGrid(train_df, col='Survived', row='Pclass', size=2.2, aspect=1.6)
+#grid.map(plt.hist, 'Age', alpha=.5, bins=20)
+#grid.add_legend();
+#grid = sns.FacetGrid(train_df, row='Embarked', size=2.2, aspect=1.6);
+#grid.map(sns.pointplot, 'Pclass', 'Survived', 'Sex', palette='deep');
+#grid.add_legend();
+#grid = sns.FacetGrid(train_df, row='Embarked', col='Survived', size=2.2, aspect=1.6)
+#grid.map(sns.barplot, 'Sex', 'Fare', alpha=.5, ci=None)
+#grid.add_legend()
+#plt.show()
+
+#5 --------------------------Manipulation of data-----------------------------------------
